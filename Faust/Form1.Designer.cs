8c5ee19db94a4ab9bbc9b6a89def5271a5308800
Xttiriey <65865283+Xttiriey@users.noreply.github.com>
2021-08-11 13:53:21 +0300

    Added code files and software

---
+++ Faust/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Faust
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.hiddenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.normalToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hiddenToolStripMenuItem,
+            this.normalToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 92);
+            // 
+            // hiddenToolStripMenuItem
+            // 
+            this.hiddenToolStripMenuItem.Name = "hiddenToolStripMenuItem";
+            this.hiddenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.hiddenToolStripMenuItem.Text = "Hidden";
+            this.hiddenToolStripMenuItem.Click += new System.EventHandler(this.hiddenToolStripMenuItem_Click);
+            // 
+            // normalToolStripMenuItem
+            // 
+            this.normalToolStripMenuItem.Name = "normalToolStripMenuItem";
+            this.normalToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.normalToolStripMenuItem.Text = "Normal";
+            this.normalToolStripMenuItem.Click += new System.EventHandler(this.normalToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem hiddenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem normalToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+    }
+}
+
---
+++ Faust/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Faust
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static string _DESCTOPPATH = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        private static readonly string ExecutablePath = Assembly.GetEntryAssembly().Location;
+        private static readonly string StartupDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
+        private static readonly string InstallLocation = Path.Combine(StartupDirectory, Path.GetFileName(ExecutablePath));
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InstallToStartup();
+            notifyIcon1.BalloonTipTitle = "Faust";
+            notifyIcon1.BalloonTipText = "The program is minimized in the tray ";
+            notifyIcon1.Text = "Faust";
+            this.Hide();
+            notifyIcon1.Visible = true;
+            notifyIcon1.ShowBalloonTip(1000);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+            // DIRECTORIES
+
+            string[] _filesNormal = Directory.GetFiles(_DESCTOPPATH, "*");
+            string[] _directoriesNormal = Directory.GetDirectories(_DESCTOPPATH, "*", SearchOption.AllDirectories);
+
+            // REMOVE desktop.ini
+
+            for (int i = 0; i < _filesNormal.Length; i++)
+            {
+                if (_filesNormal[i] == _DESCTOPPATH + "\\desktop.ini")
+                {
+                    Remove_desctop_ini(ref _filesNormal, i);
+                }
+            }
+
+            // FILES
+
+            foreach (string _filesConvert in _filesNormal)
+            {
+
+                FileInfo _thisFile = new FileInfo(_filesConvert);
+
+                if (_thisFile.Attributes.HasFlag(FileAttributes.Normal))
+                {
+                    _thisFile.Attributes = FileAttributes.Hidden;
+                }
+                else
+                {
+                    _thisFile.Attributes = FileAttributes.Normal;
+                }
+
+            }
+
+            // FOLDERS
+
+            foreach (string _directoriesConvert in _directoriesNormal)
+            {
+
+                DirectoryInfo _thisDirectory = new DirectoryInfo(_directoriesConvert);
+
+                if (!_thisDirectory.Attributes.HasFlag(FileAttributes.Hidden))
+                {
+                    _thisDirectory.Attributes = FileAttributes.Hidden;
+                }
+                else
+                {
+                    _thisDirectory.Attributes = FileAttributes.Normal;
+                }
+            }
+        }
+
+        public static void InstallToStartup()
+        {
+            if (!File.Exists(InstallLocation))
+            {
+                File.Copy(ExecutablePath, InstallLocation);
+            }
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void hiddenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            string[] _filesNormal = Directory.GetFiles(_DESCTOPPATH, "*");
+            string[] _directoriesNormal = Directory.GetDirectories(_DESCTOPPATH, "*", SearchOption.AllDirectories);
+
+            // REMOVE desktop.ini
+
+            for (int i = 0; i < _filesNormal.Length; i++)
+            {
+                if (_filesNormal[i] == _DESCTOPPATH + "\\desktop.ini")
+                {
+                    Remove_desctop_ini(ref _filesNormal, i);
+                }
+            }
+
+            // HIDE FILES
+
+            foreach (string _filesConvert in _filesNormal)
+            {
+                FileInfo _thisFile = new FileInfo(_filesConvert);
+                _thisFile.Attributes = FileAttributes.Hidden;
+            }
+
+            // HIDE FOLDERS
+
+            foreach (string _directoriesConvert in _directoriesNormal)
+            {
+                DirectoryInfo _thisDirectory = new DirectoryInfo(_directoriesConvert);
+                _thisDirectory.Attributes = FileAttributes.Hidden;
+            }
+        }
+
+        private void normalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            string[] _filesNormal = Directory.GetFiles(_DESCTOPPATH, "*");
+            string[] _directoriesNormal = Directory.GetDirectories(_DESCTOPPATH, "*", SearchOption.AllDirectories);
+
+            // REMOVE desktop.ini
+
+            for (int i = 0; i < _filesNormal.Length; i++)
+            {
+                if (_filesNormal[i] == _DESCTOPPATH + "\\desktop.ini")
+                {
+                    Remove_desctop_ini(ref _filesNormal, i);
+                }
+            }
+
+            // NORMAL FILES
+
+            foreach (string _filesConvert in _filesNormal)
+            {
+                FileInfo _thisFile = new FileInfo(_filesConvert);
+                _thisFile.Attributes = FileAttributes.Normal;
+            }
+
+            // NORMAL FOLDERS
+
+            foreach (string _directoriesConvert in _directoriesNormal)
+            {
+                DirectoryInfo _thisDirectory = new DirectoryInfo(_directoriesConvert);
+                _thisDirectory.Attributes = FileAttributes.Normal;
+            }
+        }
+
+        private static void Remove_desctop_ini(ref string[] array, int index)
+        {
+            string[] newArray = new string[array.Length - 1];
+
+            for (int i = 0; i < index; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            for (int i = index + 1; i < array.Length; i++)
+            {
+                newArray[i - 1] = array[i];
+            }
+
+            array = newArray;
+        }
+    }
+}
